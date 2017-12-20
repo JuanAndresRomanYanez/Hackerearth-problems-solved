b5f0a4e454282ed97243abab3465f969b78c764b
Juan Andrés Román Yáñez <andy_blest@hotmail.com>
2017-12-20 17:14:07 -0400

    Create Toggle String.cpp

---
+++ Toggle String.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+char a[101];
+int main(){
+    scanf("%s", a);
+    for(int i=0;i<strlen(a);++i){
+        if(a[i]>=65 && a[i]<=90)printf("%c", a[i]+32);
+        else printf("%c", a[i]-32);
+    }
+}
