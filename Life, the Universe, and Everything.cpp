e37e102382b97b3158760ee2dd4e938be58ffb01
Juan Andrés Román Yáñez <andy_blest@hotmail.com>
2017-12-21 17:44:49 -0400

    Create Life, the Universe, and Everything.cpp

---
+++ Life, the Universe, and Everything.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#define optimizar_io ios_base::sync_with_stdio(0);cin.tie(0);
+using namespace std;
+long long x;
+int main(){
+    optimizar_io
+    while( (cin>>x) && (x!=42))cout<<x<<"\n";
+}
